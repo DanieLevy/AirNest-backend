2dc9343263082ae85dcae4612b568a799b7e857d
Daniel Levy <daniellofficial@gmail.com>
2023-11-06 22:49:14 +0200

    Update MongoDB connection string and database name in prod config

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 export default {
   dbURL:
     process.env.MONGO_URL ||
-    'mongodb+srv://peleg:G1gJ8B8zPcrNWQ3K@cluster0.f1mjs59.mongodb.net/?retryWrites=true&w=majority',
-  dbName: process.env.DB_NAME || 'tester_db',
+    'mongodb+srv://peleg:G1gJ8B8zPcrNWQ3K@airnest.tomb5ll.mongodb.net/',
+  dbName: process.env.DB_NAME || 'airnest',
 }
